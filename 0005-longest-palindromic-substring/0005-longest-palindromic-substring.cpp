a878b09a1fc58943584fd873abc02ce312454916
Adarsh <137900871+hacketthadwin@users.noreply.github.com>
2024-11-29 22:06:29 +0530

    0005-longest-palindromic-substring.cpp

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
       string t = ""; 
+//reduced time complexity check of palindrome 
     bool isPalindrome(const string &s, int start, int end) {
         while (start < end) {
             if (s[start] != s[end]) {
@@ -11,7 +12,7 @@ public:
         }
         return true;
     }
-
+//this code is an example of iterative dp where we used continue function to skip the unnecessary states
     string longestPalindrome(string s) {
         for (int i = 0; i < s.size(); i++) {
             for (int j = i; j < s.size(); j++) {
