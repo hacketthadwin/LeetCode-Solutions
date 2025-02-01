e1fef218dc34f35b5f294f4eed561283e0897d41
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2025-02-01 19:09:56 +0530

    Time: 0 ms (100%), Space: 10.5 MB (62.29%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,21 @@
 class Solution {
+    //the bottom up approach of this code is very strange, common, but difficult to understand
 public:
-int dp[400];
-int solve(vector<int>& nums,int index,int n)
-{
-    //base case
-    if (index >= n) return 0;
-    //cache check
-    if(dp[index]!=-1)return dp[index];
-    //compute and transition
-        int robCurrent = nums[index] + solve(nums, index + 2, n); // Rob current house
-        int skipCurrent = solve(nums, index + 1, n);             // Skip current house
-
-    return dp[index]=max(robCurrent, skipCurrent);
-
-}
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,0,nums.size());
-        
+        //this literal just like O(1) code, technically O(n) but n=100, so does it matter?
+        vector<int>t(nums.size()+1,0);
+        //t[i] denotes maximum money till ith house
+        t[1]=nums[0];
+        for(int i=2;i<=nums.size();i++)
+        {
+            //stealing from current house and checking the maximum value till (i-2)th house, because we cannot check from (i-1)th house
+            int steal=nums[i-1]+t[i-2];
+            //same for skip, choosing the last house value only,thus skipping current house value
+            int skip=t[i-1];
+
+            //choosing maximum value till now
+            t[i]=max(steal,skip);
+        }
+        return t[nums.size()];
     }
 };
