1d34e76578b7b88fb6625ce9d18694e707f812ae
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2025-05-23 00:44:12 +0530

    Time: 15 ms (95.24%), Space: 13.5 MB (78.91%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int ultarec(string text1, string text2)
+    {
+        int ultadp[1001][1001];
+        for(int i=0;i<1001;i++)
+        {
+            ultadp[0][i]=0;
+        }
+        for(int i=0;i<1001;i++)
+        {
+            ultadp[i][0]=0;
+        }
+
+        for(int i=1;i<=text1.size();i++)
+        {
+            for(int j=1;j<=text2.size();j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                {
+                    ultadp[i][j]=1+ultadp[i-1][j-1];
+                }
+                else{
+                    ultadp[i][j]=max(ultadp[i-1][j],ultadp[i][j-1]);
+                }
+            }
+        }
+
+        return ultadp[text1.size()][text2.size()];
+
+    }
+    int minInsertions(string s) {
+             string t=s;
+     reverse(t.begin(),t.end());   
+     return (t.size()-ultarec(s,t));
+        
+    }
+};
