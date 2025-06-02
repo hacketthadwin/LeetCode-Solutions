a75fbd5b14d2b32491cd81605731b9e28ed677fe
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2025-06-02 22:43:20 +0530

    Time: 180 ms (6.53%), Space: 171.4 MB (6.02%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,47 +1,38 @@
 class Solution {
-public:   
-    bool check(string t)
+public:
+//we will solve this problem using matrix chain multiplication approach
+    vector<vector<string>>finans;
+    bool ispalindrome(string t)
     {
         for(int i=0;i<t.size();i++)
         {
-            if(t[i]!=t[t.size()-i-1])
-            {
-                return false;
-            }
+            if(t[i]!=t[t.size()-i-1])return false;
         }
         return true;
     }
-    void rec(int level,string t,vector<vector<string>>&ans,vector<string>&temp)   
+    void rec(string s,int i,int j,vector<string>tempans)
     {
-
-        //you can add pruning case but its not necessary
-        //base case
-        if(level==t.size())     //removed level==t.size()-1 because by this condition i cannot check the last element of t string
+        if(i>j)
         {
-            ans.push_back(temp);
+            finans.push_back(tempans);
             return;
         }
-         // Try all possible substrings starting from 'level'
-        for (int i = level; i < t.size(); i++) {
-            string substring = t.substr(level, i - level + 1);
-
-            // If the substring is a palindrome, add it to temp and recurse
-            if (check(substring)) {
-                temp.push_back(substring);
-                rec(i + 1, t, ans, temp);  // Recurse with the next level after the palindrome
-                temp.pop_back();  // Backtrack by removing the last substring added to temp
+        for(int k=i;k<=j;k++)   //by just one change that is k<j to k<=j i got from empty answer to correct answer, so remember this debug thing...okay?
+        {
+            string t= s.substr(i, k - i + 1);
+            if(ispalindrome(t))
+            {
+                tempans.push_back(t);
+                rec(s, k + 1, j, tempans);
+                tempans.pop_back();
             }
         }
-        
-        return;  
+
     }
     
     vector<vector<string>> partition(string s) {
-        vector<vector<string>>ans;
-        vector<string>temp;
-        string p;
-        rec(0,s,ans,temp);
-        return ans;
-        
+        vector<string>tempans;
+        rec(s,0,s.size()-1,tempans);
+        return finans;
     }
 };
