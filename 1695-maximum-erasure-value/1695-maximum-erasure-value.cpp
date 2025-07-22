c4a78e8bf23c9b8ab15b585af8a63ca5c164ff60
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2025-07-22 16:15:21 +0530

    Time: 191 ms (39.14%), Space: 110.1 MB (61.57%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>prefix(n+1,0);
+        prefix[1]=nums[0];
+        for(int i=2;i<=n;i++)
+        {
+            prefix[i]=prefix[i-1]+nums[i-1];
+        }
+        map<int,int>mp;
+        int max_sum=0;
+        for(int i=0,j=0;j<n && i<n;j++)
+        {
+            if(!mp.count(nums[j]))
+            {
+                mp[nums[j]]=j;
+            }
+            else
+            {
+                i=max(i,mp[nums[j]]+1);   //ok so the error was in this line
+                //i didn't thought that i can also move backwards with this logic and if it moved backwards then multiple elements will get counted, so the score will increase;
+
+                mp[nums[j]]=j;
+            }
+            max_sum=max(max_sum,prefix[j+1]-prefix[i]);
+        }
+        return max_sum;
+    }
+};
