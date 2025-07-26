0cfcb03a51189c7ba11851a6fad1055854c12c54
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2025-07-26 14:20:45 +0530

    Time: 43 ms (49.28%), Space: 16.3 MB (73.8%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,50 +1,47 @@
 class Solution {
-    string s1, s2;
-    int n;
-    int dp[31][31][31][31]; // dp[i1][j1][i2][j2]
+public:
+    int dp[961][961]; // 31*31 = 961, as per constraints
 
-    bool rec(int i1, int j1, int i2, int j2) {
-        if (dp[i1][j1][i2][j2] != -1)
-            return dp[i1][j1][i2][j2];
+    bool solve(string& s1, string& s2, int i1, int j1, int i2, int j2) {
+        int len = j1 - i1 + 1;
+        int key1 = i1 * 31 + j1;
+        int key2 = i2 * 31 + j2;
 
-        int len = j1 - i1;
+        int& val = dp[key1][key2];
+        if (val != -1) return val;
 
-        // Base case: if substrings are equal
-        if (s1.substr(i1, len) == s2.substr(i2, len))
-            return dp[i1][j1][i2][j2] = 1;
+        string a = s1.substr(i1, len);
+        string b = s2.substr(i2, len);
 
-        // Pruning: if they don't have same letters
-        string sub1 = s1.substr(i1, len), sub2 = s2.substr(i2, len);
-        sort(sub1.begin(), sub1.end());
-        sort(sub2.begin(), sub2.end());
-        if (sub1 != sub2)
-            return dp[i1][j1][i2][j2] = 0;
+        if (a == b) return val = true;
 
-        // Try all splits
+        string sorted_a = a;
+        string sorted_b = b;
+        sort(sorted_a.begin(), sorted_a.end());
+        sort(sorted_b.begin(), sorted_b.end());
+        if (sorted_a != sorted_b) return val = false;
+
         for (int k = 1; k < len; k++) {
             // No swap
-            if (rec(i1, i1 + k, i2, i2 + k) &&
-                rec(i1 + k, j1, i2 + k, j2))
-                return dp[i1][j1][i2][j2] = 1;
+            if (
+                solve(s1, s2, i1, i1 + k - 1, i2, i2 + k - 1) &&
+                solve(s1, s2, i1 + k, j1, i2 + k, j2)
+            ) return val = true;
 
-            // Swap
-            if (rec(i1, i1 + k, j2 - k, j2) &&
-                rec(i1 + k, j1, i2, j2 - k))
-                return dp[i1][j1][i2][j2] = 1;
+            // With swap
+            if (
+                solve(s1, s2, i1, i1 + k - 1, j2 - k + 1, j2) &&
+                solve(s1, s2, i1 + k, j1, i2, j2 - k)
+            ) return val = true;
         }
 
-        return dp[i1][j1][i2][j2] = 0;
+        return val = false;
     }
 
-public:
-    bool isScramble(string _s1, string _s2) {
-        s1 = _s1;
-        s2 = _s2;
-        n = s1.size();
-
-        if (n != s2.size()) return false;
-
+    bool isScramble(string s1, string s2) {
+        if (s1 == s2) return true;
+        int n = s1.length();
         memset(dp, -1, sizeof(dp));
-        return rec(0, n, 0, n);
+        return solve(s1, s2, 0, n - 1, 0, n - 1);
     }
 };
