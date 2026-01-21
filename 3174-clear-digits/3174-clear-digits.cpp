ea70e5767d257e89091419e99e8825ce06fed521
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-01-21 20:35:53 +0530

    Time: 0 ms (100%), Space: 8.4 MB (67.22%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -17,5 +17,7 @@ public:
             }
        }
        return ans;
+       //what i learnt from this
+        //whenever you get bug of \u0000 or any null char which you are not expecting, it is difficult to remove them too, but the bug is that you are probably iterating to the end of string like i<=s.size(), this was my bug here
     }
 };
