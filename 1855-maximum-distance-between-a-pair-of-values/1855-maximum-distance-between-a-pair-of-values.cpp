82bac5e294d3da4148f1964d4aaf65480ff9d7a0
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-04-22 17:27:15 +0530

    Time: 0 ms (100%), Space: 102.2 MB (50.93%) - LeetHub

---
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        //i have been given two non increasing nums, what the hell, this one's too easy
+        // reverse(nums1.begin(),nums1.end());
+        // reverse(nums2.begin(),nums2.end());
+        // //now the numbers that i want to find became n-i-1 and m-j-1, i want to find difference of these 2
+        // //i want nums2 value to be bigger than mine, hence i will do just lower bound in num2 to find the first value which is bigger than nums1 and then if there is any number, i will directly take the last index
+        // for(int i=0;i<nums1.size();i++)
+        // {
+
+        // }
+
+        //but this can be simply solved using two pointers as
+        int maxi=0;
+        for(int i=0,j=0;i<nums1.size() && j<nums2.size();)
+        {
+            if(nums1[i]<=nums2[j])
+            {
+                if(j>=i)maxi=max(j-i,maxi);
+                j++;
+            }
+            else
+            {
+               if(i<j)
+               {
+                     i++;
+               }
+               else
+               {
+                    i++;
+                    j++;
+               }
+            }
+            
+        }
+        return maxi;
+    }
+};
