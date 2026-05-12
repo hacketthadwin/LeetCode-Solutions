97a9ce933c1f52175deab4b3985ac8809016a1db
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-05-12 15:13:01 +0530

    Time: 55 ms (43.2%), Space: 110.5 MB (22.82%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -15,9 +15,11 @@ bool check(int m,vector<pair<int,pair<int,int>>>&v)
     }
 
     return true;
-
 }
     int minimumEffort(vector<vector<int>>& tasks) {
+        //ok so the trick for this problem was sort it reversely in terms of min-actual, i didn't noticed it first, and i sorted it based on minimum value but it failed on second test case, the proof of this greedy method was
+        //we were doing max(m1,a1+m2) for first and max(m2,a2+m1) for second for comparsion between two elements (the sorting algo method)
+        //simplifying it we get the relation as m1-a1<=m2-a2
         vector<pair<int,pair<int,int>>>v;
         for(int i=0;i<tasks.size();i++)
         {
