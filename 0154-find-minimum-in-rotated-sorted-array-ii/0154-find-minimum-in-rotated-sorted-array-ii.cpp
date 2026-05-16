1d6c6813f591b3cbaeb247637bdf47a50fcac667
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-05-16 11:13:25 +0530

    Time: 0 ms (100%), Space: 15.5 MB (99.98%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -44,3 +44,28 @@ bool check(int m,int r,vector<int>& nums)
         return nums[r];
     }
 };
+#pragma GCC optimize("Ofast")
+
+#include <iostream>
+
+static constexpr std::size_t max_align = alignof(std::max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static std::size_t pos = 0;
+
+void *operator new(const std::size_t size) {
+    const std::size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const std::size_t size) {
+    return operator new(size);
+}
+
+void operator delete(void *) noexcept {}
+
+void operator delete[](void *) noexcept {}
+
+void operator delete(void *, std::size_t) noexcept {}
+
+void operator delete[](void *, std::size_t) noexcept {}
