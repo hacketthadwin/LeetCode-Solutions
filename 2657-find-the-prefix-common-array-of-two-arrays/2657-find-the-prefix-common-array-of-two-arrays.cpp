d83442bf83502dd7c3cda426fdc4a2ba2c5f2d42
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-05-20 16:26:27 +0530

    Time: 10 ms (43.06%), Space: 89.3 MB (17.84%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-                int n = A.size();
-        vector<int> prefixCommonArray(n);
-
-        // Loop through each index to calculate common elements for each prefix
-        for (int currentIndex = 0; currentIndex < n; ++currentIndex) {
-            int commonCount = 0;
-
-            // Compare elements in A and B within the range of current prefix
-            for (int aIndex = 0; aIndex <= currentIndex; ++aIndex) {
-                for (int bIndex = 0; bIndex <= currentIndex; ++bIndex) {
-                    // Check if elements match, and count if they do
-                    if (A[aIndex] == B[bIndex]) {
-                        ++commonCount;
-                        break;
-                    }
-                }
-            }
-
-            // Store the count of common elements for the current prefix
-            prefixCommonArray[currentIndex] = commonCount;
+        int n=A.size();
+        map<int,int>mp;
+        vector<int>v(51,0);
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        {
+            mp[A[i]]++;
+            mp[B[i]]++;
+            // i+1 is size, and mp.size is total size till now, 
+            ans.push_back((2*(i+1))-mp.size());
         }
+        return ans;
 
-        // Return the final array with counts of common elements in each prefix
-        return prefixCommonArray;
+
+        
     }
 };
