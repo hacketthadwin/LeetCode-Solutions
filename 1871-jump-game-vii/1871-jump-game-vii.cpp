38d8c6fb5cc4f1bf367ccfd4614d26e6f7f94b76
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-05-25 14:02:38 +0530

    Time: 12 ms (46.82%), Space: 24.7 MB (42.86%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -28,29 +28,57 @@ public:
 
         //ok so if this is giving mle even after memoisation, then it means it expects some other moves, so i will have to check from i+maxJump-minJump to i+maxJump, then i guess it can give correct answer
         //i was missing the reachable idea, it was a good learning question, that sometimes...choosing the iterative dp may work in miracles
-        int dp[s.size()+1];
-        memset(dp,0,sizeof(dp));
-        dp[0]=1;
-        int reachable=0;
-        for(int i=1;i<s.size();i++)
+        // int dp[s.size()+1];
+        // memset(dp,0,sizeof(dp));
+        // dp[0]=1;
+        // int reachable=0;
+        // for(int i=1;i<s.size();i++)
+        // {
+        //     //the below is checking from minJump
+        //     if(i-minJump>=0 && dp[i-minJump])
+        //     {
+        //         reachable++;
+        //     }
+        //     //the below is leaving the reachability from the out of range previous index
+        //     if(i-maxJump-1>=0 && dp[i-maxJump-1])
+        //     {
+        //         reachable--;
+        //     }
+
+        //     if(s[i]=='0' && reachable>0)
+        //     {
+        //         dp[i]=1;
+        //     }
+        // }
+
+        // return dp[s.size()-1];
+
+
+        queue<int>q; //this will take indicex
+        q.push(0);
+        vector<bool>visited(s.size(),false);
+        int farthest_visited=0;
+        visited[0]=true;
+        while(!q.empty())
         {
-            //the below is checking from minJump
-            if(i-minJump>=0 && dp[i-minJump])
+            int indx=q.front();
+            // farthest_visited=max(indx,farthest_visited+1);
+            if(indx==s.size()-1)return true;
+            q.pop();
+            
+            for(int i=max(farthest_visited,indx+minJump);i<=min((int)s.size()-1,indx+maxJump);i++)
             {
-                reachable++;
+                if(s[i]=='0' && !visited[i])
+                {
+                    q.push(i);
+                    visited[i]=true;
+                }
             }
-            //the below is leaving the reachability from the out of range previous index
-            if(i-maxJump-1>=0 && dp[i-maxJump-1])
-            {
-                reachable--;
-            }
-
-            if(s[i]=='0' && reachable>0)
-            {
-                dp[i]=1;
-            }
+            farthest_visited=max(indx+maxJump,farthest_visited+1); //this was the main optimisation, we should not update farthest index before loop, we should not update farthest index in the loop, we should update the farthest index after the loop, like it is the max index which has been explored,
+            //so if i have already saw index till indx+maxJump, then farthest index should be continued from that only
         }
+        return false;
 
-        return dp[s.size()-1];
+
     }
 };
