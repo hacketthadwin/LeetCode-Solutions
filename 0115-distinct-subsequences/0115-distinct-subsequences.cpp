6b42dc865658fb06b9811c60871e644f4db6ff40
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-06-06 18:50:24 +0530

    Time: 39 ms (20.13%), Space: 44.2 MB (40.38%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+// int dp[1001][1001];
+// int rec(int idx,int jdx, string&s, string&t)
+// {
+//     if(jdx==t.size()) return 1;
+//     if(idx==s.size()) return 0;
+//
+//     if(dp[idx][jdx]!=-1) return dp[idx][jdx];
+//
+//     int curr_ans=0;
+//     for(int i=idx;i<s.size();i++)
+//     {
+//         if(s[i]==t[jdx])
+//         {
+//             curr_ans+=rec(i+1,jdx+1,s,t);
+//         }
+//     }
+//     return dp[idx][jdx]=curr_ans;
+// }
+
+    int numDistinct(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+
+        vector<vector<unsigned long long>> dp(n+1, vector<unsigned long long>(m+1,0));
+
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][m]=1;
+        }
+
+        for(int j=m-1;j>=0;j--)
+        {
+            for(int i=n-1;i>=0;i--)
+            {
+                dp[i][j]=dp[i+1][j]; //skip it
+
+                if(s[i]==t[j]) //take it
+                {
+                    dp[i][j]+=dp[i+1][j+1];
+                }
+            }
+        }
+
+        return (int)dp[0][0];
+    }
+};
