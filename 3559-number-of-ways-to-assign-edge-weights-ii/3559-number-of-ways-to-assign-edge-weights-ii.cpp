0d0273c1fcd0b700705155cda67dfe69e8dec72c
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-06-11 17:14:56 +0530

    Time: 571 ms (24.18%), Space: 425.3 MB (12.09%) - LeetHub

---
+++ 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.cpp
@@ -0,0 +1,113 @@
+class Solution {
+public:
+vector<vector<int>>adj;
+int N;
+int M=30;
+const long long MOD=1e9+7;
+vector<int>dep;
+vector<vector<int>>par;
+void dfs(int curr,int parent)
+{
+    if(parent!=-1)dep[curr]=dep[parent]+1;
+    par[curr][0]=parent;
+    for(int i=1;i<M;i++)
+    {
+        par[curr][i]=par[par[curr][i-1]][i-1];
+    }
+    for(auto x:adj[curr])
+    {
+        if(x!=parent)
+        {
+            dfs(x,curr);
+        }
+    }
+}
+int kth_parent(int curr,int k)
+{
+    int ans=curr;
+    for(int i=0;i<M;i++)
+    {
+        if((k>>i)&1)
+        {
+            ans=par[ans][i];
+        }
+    }
+    return ans;
+}
+int lca(int u,int v)
+{
+    if(dep[u]<dep[v])swap(u,v);
+    int k=dep[u]-dep[v];
+    //then lift v
+    u=kth_parent(u,k);
+    //now all we got to do is find the lca of both u and v
+    if(u==v)return u;
+    for(int i=M-1;i>=0;i--)  //always take lca from highest i to 0
+    {
+        if(par[u][i]!=par[v][i])
+        {
+            u=par[u][i];
+            v=par[v][i];
+        }
+    }
+
+    return par[u][0];
+}
+int dist_between(int u,int v)
+{
+    return dep[u]+dep[v]-(2*dep[lca(u,v)]);
+}
+
+long long binpow(long long a, long long b)
+{
+    a %= MOD;
+    long long res = 1;
+    while(b > 0)
+    {
+        if(b & 1)
+            res = (res * a) % MOD;
+
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+    vector<int> assignEdgeWeights(vector<vector<int>>& edges, vector<vector<int>>& queries) {
+                //so basically i just need to find the maximum depth...after that it becomes maths problem...which is number of ways to assign 1 and 2 at n place where n is maximum depth so that we get sum as odd...of course the answer can be very high that's why we get it as modulo 1e9+7
+        //if i solve the second version of this problem..this version will automatically gets solved...so basically i will need lca for the second version...i will get lca for the second version...then i will easily find the distance between both nodes...after the node distance...all it is remaining is again maths...first let's figure out the maths for this
+        //so what will be the way to count...see i want number of ways get odd..
+        //yeah so after observing few test cases..i found out to be just (1<<(n-1)), now i just have to take the modulo of this
+        //this can be done in max to max o(log n)..so mathematics done..graph remaining
+        N=edges.size()+1;
+        dep.resize(N);
+        par.resize(N,vector<int>(M));
+        adj.resize(N);
+        for(int i=0;i<edges.size();i++)
+        {
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj[u-1].push_back(v-1);
+            adj[v-1].push_back(u-1);
+        }
+        dep[0]=-1;
+        for(int i=0;i<M;i++)
+        {
+            par[0][i]=0;
+        }
+        dfs(0,0);
+        vector<int>ans;
+        for(int i=0;i<queries.size();i++)
+        {
+            int u=queries[i][0];
+            int v=queries[i][1];
+            int maxi=dist_between(u-1,v-1);
+            if(maxi==0)
+            {
+                ans.push_back(0);
+                continue;
+            }
+            ans.push_back(binpow(2,maxi-1));
+        }
+        return ans;
+    }
+};
