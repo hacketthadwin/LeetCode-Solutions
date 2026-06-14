3f361fe880ad148a8aaf48b97668af308c079eff
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-06-15 00:01:53 +0530

    Time: 123 ms (5.13%), Space: 163.4 MB (5.01%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -8,20 +8,39 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ //think like this 1->2->3->4
+ //my base case returned 4
+ //my temp took 4
+ //then in that 4..i attach 3 to next of 4
+ //so 4->3
+ //now...ok so my reverse linked list will begin from new_head and not temp
+ //small debugging session everytime
 class Solution {
 public:
+    ListNode* new_head= nullptr;
+    ListNode* reverse(ListNode* head)
+    {
+        if(head->next==nullptr)
+        {
+            //base case
+            new_head=new ListNode(head->val);
+            return new_head;
+        }
+
+        ListNode* temp = reverse(head->next);
+        temp->next=new ListNode(head->val);
+        return temp->next;
+    }
     int pairSum(ListNode* head) {
-        vector<int>arr;
-        while(head!=nullptr)
-        {
-            arr.push_back(head->val);
-            head=head->next;
-        }
+       ListNode* temp= reverse(head);
         int maxi=INT_MIN;
-        for(int i=0;i<arr.size();i++)
-        {
-            maxi=max(arr[i]+arr[arr.size()-1-i],maxi);
-        }
-        return maxi;
+       while(head!=nullptr)
+       {
+            maxi=max(maxi, head->val+new_head->val);
+            head=head->next;
+            new_head=new_head->next;
+       }
+
+       return maxi;
     }
 };
