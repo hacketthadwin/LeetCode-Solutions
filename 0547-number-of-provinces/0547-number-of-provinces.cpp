90e3f04510d4faec5c298a33ff8adba642851070
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-07-02 13:03:42 +0530

    Time: 0 ms (100%), Space: 19.4 MB (72.46%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,46 @@
+class unionfind{
+    private:vector<int>p,rank;
+    public:
+    unionfind(int n)
+    {
+        rank.assign(n,0);p.assign(n,0);
+        iota(p.begin(),p.end(),0);
+    }
+    int find_set(int i){return (p[i]==i) ? i : p[i]=find_set(p[i]);}
+    bool is_same_set(int i,int j){return find_set(i)==find_set(j);}
+    void union_set(int i,int j){
+        if(!is_same_set(i,j))
+        {
+            int x=find_set(i),y=find_set(j);
+            if(rank[x]>rank[y])p[y]=x;
+            else
+            {
+                p[x]=y;
+                if(rank[x]==rank[y])rank[y]++;
+            }
+        }
+    }
+};
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        unionfind DSU(n);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(isConnected[i][j]==1)
+                {
+                    DSU.union_set(i,j);
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(DSU.find_set(i)==i)cnt++;
+        }
+        return cnt;
+    }
+};
