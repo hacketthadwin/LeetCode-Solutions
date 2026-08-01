05103ff4c135f25390a31ecce974995bad0e6dba
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-08-01 22:57:42 +0530

    Time: 1514 ms (38.12%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,11 +1,7 @@
 # Write your MySQL query statement below
--- so what i see here is i need to group and then find the number of transactions..so basically i will say this requires inner join but for those which don't have any visit id in transactions and then aggregate count of each values
--- this one is good for a join problem
-SELECT tabl.customer_id,COUNT(tabl.customer_id) AS count_no_trans
-FROM
-(SELECT v.customer_id AS customer_id, t.transaction_id AS transaction_id
-FROM Visits v
-LEFT JOIN Transactions t
-ON v.visit_id=t.visit_id) tabl
-WHERE tabl.transaction_id IS NULL
-GROUP BY tabl.customer_id
+select v.customer_id, count(v.customer_id) as count_no_trans 
+from Visits v
+left join Transactions t
+on v.visit_id=t.visit_id
+where t.transaction_id is null 
+group by v.customer_id
