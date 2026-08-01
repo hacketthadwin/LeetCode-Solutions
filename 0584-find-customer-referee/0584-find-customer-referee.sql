10d7430dc4bddfd91c9644091670ffb64d70c4bc
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-08-01 21:48:49 +0530

    Time: 464 ms (88.48%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
-
-#remember, null is not represented as referee_id=null, instead we use IS NULL for finding null values
-SELECT name FROM Customer WHERE referee_id IS NULL OR referee_id!=2;
+SELECT
+    name
+FROM
+    Customer
+WHERE (referee_id != 2) OR (referee_id is null)
