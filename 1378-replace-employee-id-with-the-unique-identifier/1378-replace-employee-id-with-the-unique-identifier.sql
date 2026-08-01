cdc346f80b8524a372176c6cd6117a849f07e508
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-08-01 22:18:08 +0530

    Time: 1307 ms (44.47%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,7 +1,4 @@
 # Write your MySQL query statement below
--- this will use left join since there is every value of left table but not right
-SELECT e.name,eid.unique_id
-FROM Employees e
-LEFT JOIN
-EmployeeUNI eid
-ON e.id=eid.id
+select eu.unique_id as unique_id, e.name
+from Employees e
+left join EmployeeUNI eu on e.id=eu.id
