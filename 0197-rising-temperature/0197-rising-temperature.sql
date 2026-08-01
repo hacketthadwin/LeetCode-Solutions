9b742dd648acbfe6af69dffd80046983cf36f25d
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-08-02 00:08:16 +0530

    Time: 629 ms (11.85%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,8 +1,5 @@
 # Write your MySQL query statement below
--- this may use the lag thing..i can build a table with lag thing...then i will check if current temperature is higher than last day
-SELECT tabl.id
-FROM
-(SELECT id,temperature,LAG(temperature) OVER(ORDER BY recordDate) AS prev_temp,recordDate, LAG(recordDate) OVER(ORDER BY recordDate) AS prev_date
-FROM Weather
-) tabl
-WHERE DATEDIFF(tabl.recordDate,tabl.prev_date)=1 AND  tabl.temperature>tabl.prev_temp
+select w1.id
+from Weather w1
+inner join Weather w2
+where datediff(w1.recordDate,w2.recordDate)=1 and w1.temperature>w2.temperature
