0d70ce4aad7fbc707e510c859a827dfee905e028
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-08-02 19:52:06 +0530

    Time: 443 ms (13.49%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,12 +1,18 @@
--- Write your PostgreSQL query statement below
-SELECT e.name
-FROM 
-Employee e
-JOIN
-(SELECT managerId, COUNT(managerId) AS cnt
-FROM
-Employee
-GROUP BY managerId
-) tabl1
-ON e.id=tabl1.managerId
-WHERE tabl1.managerId=e.id AND tabl1.cnt>=5
+-- # Write your MySQL query statement below
+-- select * 
+-- from
+-- (select tabl.managerId,tabl.name
+-- from
+-- (select e1.managerId,e1.name,e1.id, count(e1.managerId) as cnt
+-- from Employee e1
+-- group by e1.managerId) tabl
+-- where tabl.cnt>=5) tabl2
+-- inner join Employee e2
+-- on e2.managerId=tabl2.managerId
+
+select e1.name
+from Employee e1
+inner join Employee e2
+on e1.id=e2.managerId
+group by e2.managerId
+having count(e2.managerId)>=5
