e823b15cdea212cf7fb65ae2e957580726af623e
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-08-02 20:20:26 +0530

    Time: 759 ms (30.77%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,9 +1,11 @@
--- Write your PostgreSQL query statement below
--- so actually i dont care about time-stamp of signups
-SELECT s.user_id, ROUND(AVG(CASE WHEN action='confirmed' THEN 1 ELSE 0 END),2) AS confirmation_rate 
-FROM 
-Signups s
-LEFT JOIN
-Confirmations c
-ON s.user_id=c.user_id
-GROUP BY s.user_id
+# Write your MySQL query statement below
+select s.user_id, ifnull(round(sum(c.action = 'confirmed')/count(*),2),0.00) as confirmation_rate 
+from Signups s
+left join Confirmations c
+on s.user_id=c.user_id
+group by s.user_id
+-- group by s.user_id
+-- having c.action = 'confirmed'
+-- 3 - {timeout,timeout}
+-- 7- {confirmed,confirmed,confirmed}
+-- 2 - {confirmed,timeout}
