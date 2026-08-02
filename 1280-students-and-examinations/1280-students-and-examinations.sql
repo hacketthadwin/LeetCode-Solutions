3efa62c1b219034d5d104b1e61a902dcc8d949c5
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-08-02 19:23:06 +0530

    Time: 1380 ms (9.87%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,20 +1,7 @@
--- Write your PostgreSQL query statement below
--- this will use coalase too..as for null values..we will fill it with 0
--- so first i will have to do is for each student...make a table for each subject...then for each student id i will need to count its occurence for each subject...then will need to join it..and for null values...i will replace null values with 0..this is my approach for this
--- NOW I JUST NEED TO FIND COUNT FOR EACH, so i will do group by
-SELECT tabl1.student_id,tabl1.student_name,tabl1.subject_name,COALESCE(tabl2.attended_exams, 0) AS attended_exams
-FROM
-(SELECT * 
-FROM Students st
-CROSS JOIN
-Subjects sb
-) tabl1
-LEFT JOIN
-(
-SELECT *, COUNT(e.student_id) AS attended_exams 
-FROM Examinations e
-GROUP BY e.subject_name,e.student_id
-) tabl2
-ON tabl1.student_id=tabl2.student_id
-AND tabl1.subject_name=tabl2.subject_name
-ORDER BY tabl1.student_id,tabl1.subject_name ASC
+# Write your MySQL query statement below
+select st.student_id,st.student_name,sb.subject_name, count(e.subject_name) as attended_exams 
+from Students st
+cross join Subjects sb
+left join Examinations e on e.student_id=st.student_id and e.subject_name=sb.subject_name
+group by st.student_id,st.student_name,sb.subject_name
+order by st.student_id,sb.subject_name
