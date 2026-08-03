01a2e19ab1c91496901413045a619f23d78cb7f3
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-08-03 22:50:32 +0530

    Time: 730 ms (59.67%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -4,3 +4,7 @@ from Sales
 where (product_id,year) in (select product_id,min(year)
 from Sales
 group by product_id)
+
+-- The year in (product_id, year) belongs to the outer query or the inner query?
+--  Answer: It belongs to the outer query.
+-- and we cannot use first_year instead of year inside the argument because of order of execution
