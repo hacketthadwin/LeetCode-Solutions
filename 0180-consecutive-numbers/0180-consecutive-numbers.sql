40df154c727398bf2842e3254cefe8fb789abe66
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-08-04 21:31:46 +0530

    Time: 599 ms (47.24%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,10 +1,15 @@
 # Write your MySQL query statement below
+-- select distinct l1.num as ConsecutiveNums 
+-- from Logs l1
+-- left join
+-- Logs l2
+-- on l1.id=l2.id -1
+-- left join
+-- Logs l3
+-- on l2.id=l3.id -1
+-- where l1.num=l2.num and l2.num=l3.num
 select distinct l1.num as ConsecutiveNums 
-from Logs l1
-left join
-Logs l2
-on l1.id=l2.id -1
-left join
-Logs l3
-on l2.id=l3.id -1
-where l1.num=l2.num and l2.num=l3.num
+from Logs l1, Logs l2, Logs l3
+where l1.id=l2.id -1 and l2.id=l3.id -1 and l1.num=l2.num and l2.num=l3.num
+
+-- if you want to compare rows inside table you need 3 seperate tables
