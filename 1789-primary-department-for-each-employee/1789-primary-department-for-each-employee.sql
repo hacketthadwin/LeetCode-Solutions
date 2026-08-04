5b5e270cd330d0caabe6d813ca0e45faf8a42267
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-08-04 22:18:50 +0530

    Time: 618 ms (34.73%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,18 +1,20 @@
 # Write your MySQL query statement below
--- select employee_id,department_id
--- from Employee
--- group by employee_id
--- having count(*)=1
--- union
--- select employee_id,department_id
--- from Employee
--- where primary_flag='Y'
-
 select employee_id,department_id
 from Employee
-where (employee_id,department_id) in (
-    select  employee_id,department_id
-    from Employee
-    group by employee_id
-    having count(*)=1
-) or primary_flag='Y'
+group by employee_id
+having count(*)=1
+union
+select employee_id,department_id
+from Employee
+where primary_flag='Y'
+
+-- select employee_id,department_id
+-- from Employee
+-- where (employee_id,department_id) in (
+--     select  employee_id,department_id
+--     from Employee
+--     group by employee_id
+--     having count(*)=1
+-- ) or primary_flag='Y'
+
+-- both code are correct but i will prefer union because it general...use second one when the condition is small
