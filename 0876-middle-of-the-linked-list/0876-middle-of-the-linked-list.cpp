6b76d1e41dd6ad2a5129e01f5ff7816aaf2ffc60
Adarsh Jha <137900871+hacketthadwin@users.noreply.github.com>
2026-09-05 17:47:05 +0530

    Time: 0 ms (100%), Space: 9.9 MB (90.89%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,19 +11,17 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        // i haven't appreciated the tortoise hare method back then...but now i realise how good it is in reality
-        //this problem is really with tortoise hare solution (not intuitive at first though)
-
+        //so there are 2 methods for this...first find total size and only visit till n/2
+        //second is slow and fast pointer...the slow pointer will be moving 1 step and the fast pointer will move 2 steps
         if(head==nullptr || head->next==nullptr)return head;
-        ListNode* fast=head->next;
         ListNode* slow=head;
-        while(fast!=nullptr)
+        ListNode* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr)
         {
-            fast=fast->next;
-            if(fast!=nullptr)fast=fast->next;
-            slow=slow->next;
+            if(slow->next!=nullptr)slow=slow->next;
+            if(fast->next!=nullptr)fast=fast->next;
+            if(fast->next!=nullptr)fast=fast->next;
         }
         return slow;
-        
     }
 };
